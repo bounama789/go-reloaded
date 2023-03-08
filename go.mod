54b26462ea2b28b10bad8ae3c547d37173ed4fc2
Bounama Coulibaly <35851541+coulou800@users.noreply.github.com>
2023-03-08 08:31:06 +0000

    update

--- go.mod
+++ go.mod
@@ -2,7 +2,7 @@ module goreloaded
 
 replace piscine => ../piscine-go
 
-go 1.19
+go 1.18
 
 require piscine v0.0.0-00010101000000-000000000000
 
--- main.go
+++ main.go
@@ -4,102 +4,67 @@ import (
 	"os"
 	"piscine"
 	"strconv"
+	"strings"
 )
 
-func main()  {
+func main() {
 
-	const  (
-		hex="0123456789ABCDEF"
-		bin="01"
+	const (
+		hex = "0123456789ABCDEF"
+		bin = "01"
 	)
 
 	args := os.Args
-	options := []string{"(low)","(up)","(bin)","(hex)","(cap)"}
+	options := []string{"(low)", "(up)", "(bin)", "(hex)", "(cap)"}
+
 	if len(args) > 1 {
 		filename := args[1]
 		content, _ := os.ReadFile(filename)
-		lines := piscine.Split(string(content),"\n")
-		var n string
-		var words []string
-		for _, v := range lines {
-			words = append(words, piscine.SplitWhiteSpaces(v)...)
+		words := strings.Split(string(content), " ")
+		var ln string
 
-		}
-		for i, v := range words {
-			if v ==  options[3]{
-				n = words[i-1]
-				words[i-1] = strconv.Itoa(piscine.AtoiBase(n,hex))
-				words = piscine.RemoveStringElem(words,i)
-			}
-			if v == options[2] {
-				n = words[i-1]
-				words[i-1] = strconv.Itoa(piscine.AtoiBase(n,bin))
-				words = piscine.RemoveStringElem(words,i)
-			}
-			if v == options[1] {
-				n = words[i-1]
-				words[i-1] = piscine.ToUpper(n)
-				words = piscine.RemoveStringElem(words,i)
-			}
-			if v== options[0] {
-				n = words[i-1]
-				words[i-1] = piscine.ToLower(n)
-				words = piscine.RemoveStringElem(words,i)
-			}
-			if v == options[4] {
-				n = words[i-1]
-				words[i-1] = piscine.Capitalize(string(n[0]))+string(words[i-1][1:])
-				words = piscine.RemoveStringElem(words,i)
-			}
+		for i, word := range words {
+			if word == "\n" {
+				ln = strings.Join(words[:i], " ")
+				var option string
 
-		}
-		
-		for _, v := range words {
-			println(v)
+				for y, l := range ln {
+					if l == '(' {
+						option = getOption(ln,y)
+						println(option)
+					}
+
+					if option == options[3] {
+						ln = convertHex(ln,y)
+					}
+					
+				}
+			}
+			print(ln)
 		}
 
 	}
-		
+
 }
 
-func getPuncIndexes(content string) [][]int {
-	options := []string{"(low)","(up)","(bin)","(hex)","(cap)"}
-	punctuations := []rune{'.', ',', '!', '?', ':', ';'}
-	runes := []rune(content)
-	var dots,comma,excl,kst,col,scol,ln []int
-	var n = 0
-
-	for i := 0; i < len(runes); i++ {
-		if string(runes[i:i+len(options[0])]) == options[0] || 
-		string(runes[i:i+len(options[0])]) == options[2] ||
-		string(runes[i:i+len(options[0])]) == options[3] || 
-		string(runes[i:i+len(options[0])]) == options[4]{
-			n += 5
-		} else if string(runes[i:i+len(options[0])]) == options[1] {
-			n+=4
-		}
-
-		if (runes[i] == punctuations[0] ){
-			dots = append(dots, i-n)
-		}
-		if (runes[i] == punctuations[1] ){
-			comma = append(comma, i-n)
-		}
-		if (runes[i] == punctuations[2] ){
-			excl = append(excl, i-n)
-		}
-		if (runes[i] == punctuations[3] ){
-			kst = append(kst, i-n)
-		}
-		if (runes[i] == punctuations[4] ){
-			col = append(col, i-n)
-		}
-		if (runes[i] == punctuations[5] ){
-			scol = append(scol, i-n)
-		}
-		if (runes[i] == punctuations[5] ){
-			scol = append(scol, i-n)
+func getOption(s string,index int) string {
+	for i := index; i < len(s); i++ {
+		if s[i] == ')' {
+			return s[index:i]
 		}
 	}
+	return ""
 }
 
+func convertHex(s string, idx int) string{
+	base := "0123456789ABCDEF"
+
+	var hex string
+	for i := idx-2; i > 0; i-- {
+		if s[i] == ' ' {
+			hex = s[i:idx-1]
+			s =s[:i] + strconv.Itoa(piscine.AtoiBase(hex, base))+ s[:idx-1]
+		}
+	}
+	return s
+}
